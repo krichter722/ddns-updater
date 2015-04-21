c2343bb62c14323a2b58df34f8cf02fb466f2910
Karl-Philipp Richter <krichter722@aol.de>
2015-04-21 20:10:04 +0200

    added daemon mode

--- strato_ddns_updater.py
+++ strato_ddns_updater.py
@@ -46,52 +46,61 @@ logger.addHandler(ch)
     no_mx=("Setting MX entry is skipped if flag is set", "flag"),
     base_url=("Overwrite the base URL of the strato service", "option"),
     check_file_path=("Path to the file where the result of the last IP check is stored", "option"),
-    check_interval=("The time in seconds between two checks of the current external IP (this is time your DDNS isn't reachable in the worst case) (don't set this value too low because the service to check for external IP (currently ipecho.net) might block you if you send too many requests)", "option")
+    check_interval=("The time in seconds between two checks of the current external IP (this is time your DDNS isn't reachable in the worst case) (don't set this value too low because the service to check for external IP (currently ipecho.net) might block you if you send too many requests)", "option"),
+    daemon=("Run as daemon", "flag"),
 )
-def strato_ddns_updater(hostname, username, password, no_mx=False, base_url="https://dyndns.strato.com/nic/update", check_file_path="/tmp/strato-ddns-updater.dat", check_interval=60*15):
+def strato_ddns_updater(hostname, username, password, no_mx=False, base_url="https://dyndns.strato.com/nic/update", check_file_path="/tmp/strato-ddns-updater.dat", check_interval=60*15, daemon=False):
     try:
         check_interval_float = float(check_interval)
     except ValueError:
         raise ValueError("check interval '%s' could not be parsed as float" % (str(check_interval), ))
-    while True:
-        dyndns_response = urllib2.urlopen("http://ipecho.net/plain").readline().strip()
-        external_ip = dyndns_response
-        if os.path.exists(check_file_path):
-            check_file = open(check_file_path)
-            last_external_ip = check_file.readline().strip()
-            if last_external_ip == external_ip:
-                time.sleep(check_interval_float)
-                continue
+    def __loop__():
+        while True:
+            dyndns_response = urllib2.urlopen("http://ipecho.net/plain").readline().strip()
+            external_ip = dyndns_response
+            if os.path.exists(check_file_path):
+                check_file = open(check_file_path)
+                last_external_ip = check_file.readline().strip()
+                if last_external_ip == external_ip:
+                    time.sleep(check_interval_float)
+                    continue
+                check_file.close()
+            check_file = open(check_file_path, "w")
+            check_file.write(external_ip)
+            check_file.flush()
             check_file.close()
-        check_file = open(check_file_path, "w")
-        check_file.write(external_ip)
-        check_file.flush()
-        check_file.close()
 
-        # Create an OpenerDirector with support for Basic HTTP Authentication...
-        auth_handler = urllib2.HTTPBasicAuthHandler()
-        auth_handler.add_password(realm='strato DDSN updater',
-                                  uri=base_url,
-                                  user=username,
-                                  passwd=password)
-        opener = urllib2.build_opener(auth_handler)
-        # ...and install it globally so it can be used with urlopen.
-        urllib2.install_opener(opener)
+            # Create an OpenerDirector with support for Basic HTTP Authentication...
+            auth_handler = urllib2.HTTPBasicAuthHandler()
+            auth_handler.add_password(realm='strato DDSN updater',
+                                      uri=base_url,
+                                      user=username,
+                                      passwd=password)
+            opener = urllib2.build_opener(auth_handler)
+            # ...and install it globally so it can be used with urlopen.
+            urllib2.install_opener(opener)
 
-        if no_mx is False:
-            mx_str = "&mx=%s" % (hostname, )
-        else:
-            mx_str = ""
+            if no_mx is False:
+                mx_str = "&mx=%s" % (hostname, )
+            else:
+                mx_str = ""
 
-        request = urllib2.Request("%s?hostname=%s%s&myip=%s&mxback=NO" % (base_url, hostname, mx_str, external_ip, ))
-        # You need the replace to handle encodestring adding a trailing newline
-        # (https://docs.python.org/2/library/base64.html#base64.encodestring)
-        base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
-        request.add_header("Authorization", "Basic %s" % base64string)
-        result = urllib2.urlopen(request).readline().strip()
-        logger.info("strato update site returned '%s'" % (result, ))
-        if not result.startswith("good "):
-            raise Error("request failed because response doesn't start with 'good'")
+            request = urllib2.Request("%s?hostname=%s%s&myip=%s&mxback=NO" % (base_url, hostname, mx_str, external_ip, ))
+            # You need the replace to handle encodestring adding a trailing newline
+            # (https://docs.python.org/2/library/base64.html#base64.encodestring)
+            base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+            request.add_header("Authorization", "Basic %s" % base64string)
+            result = urllib2.urlopen(request).readline().strip()
+            logger.info("strato update site returned '%s'" % (result, ))
+            if not result.startswith("good "):
+                raise Error("request failed because response doesn't start with 'good'")
+    if daemon:
+        pid = os.fork()
+        if pid == 0:
+            __loop__()
+        # don't care about parent
+    else:
+        __loop__()
 
 # necessary to allow setup of `setuptools` `entry_points`
 def main():
